15f0c790eba303100cbc30b61837c1afdbe3ba4e
Pal Sharma <preeteshsharma10@gmail.com>
2025-08-14 10:58:04 +0530

    Bug Fixes

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,23 @@
 import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+
+app.use(express.json());
+
+const start = async() => {
+
+    const connectDB = await mongoose.connect("mongodb+srv://preeteshsharma10:Hzz9BgsWTo8H0Mnd@menteeconnect.l4jujxn.mongodb.net/")
+
+    app.listen(9080, () => {
+        console.log("Server is running on port 9080")
+    })
+}
+
+start();
