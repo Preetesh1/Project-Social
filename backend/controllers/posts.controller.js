a70754578ddca30bfffd1c93dc0366f6dda03b32
Pal Sharma <preeteshsharma10@gmail.com>
2025-09-02 23:03:43 +0530

    controllers

--- backend/controllers/posts.controller.js
+++ backend/controllers/posts.controller.js
@@ -5,3 +5,12 @@ export const activeCheck = async (req, res) => {
 
     return res.status(200).json({ message : "RUNNING" })
 }
+
+const register = async (req, res) => {
+
+    try {
+        const {name, email, password} = req.body;
+    } catch (error) {
+        return res.status(500).json({ message: error.message })
+    }
+}
